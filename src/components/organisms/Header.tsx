ccd08074daf4a23a6a1128b22107e6c25446ab27
basofiibnur <basofiibnur@gmail.com>
2024-10-11 12:03:32 +0700

    feat: add header menu

--- src/components/organisms/Header.tsx
+++ src/components/organisms/Header.tsx
@@ -1,10 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Box from '../atoms/Box';
 import Paragraph from '../atoms/Paragraph';
 import Image from '../atoms/Image';
 import Button from '../atoms/Button';
+import { AnimatePresence, motion } from 'framer-motion';
 
 const Header = () => {
+  const [showMenu, setShowMenu] = useState<boolean>(false);
+
+  const menuVariants = {
+    hidden: { opacity: 0, y: 20 },
+    visible: { opacity: 1, y: 0 },
+    exit: { opacity: 0, y: -20 },
+  };
   return (
     <header className="h-[60px] flex flex-col items-center justify-center">
       <Box className="flex w-full items-center justify-between">
@@ -12,9 +20,45 @@ const Header = () => {
           <Image src="/assets/logo.svg" alt="logo" />
         </Box>
         <Box className="flex items-center gap-8">
-          <Box className="flex items-center gap-1">
-            <Paragraph className="text-base leading-6 font-black text-white">채용</Paragraph>
-            <Image src="/assets/chevrondown.svg" alt="chevron-down" />
+          <Box className="relative">
+            <Box className="flex items-center gap-1 cursor-pointer" onClick={() => setShowMenu(!showMenu)}>
+              <Paragraph className="text-base leading-6 font-black text-white">채용</Paragraph>
+              <Image src="/assets/chevrondown.svg" alt="chevron-down" />
+            </Box>
+
+            <AnimatePresence>
+              {showMenu && (
+                <motion.div
+                  initial="hidden"
+                  animate="visible"
+                  exit="exit"
+                  variants={menuVariants}
+                  transition={{ duration: 0.3 }}
+                  className="absolute top-8 cursor-pointer z-10 w-full min-w-[240px] flex flex-col rounded-lg border border-[#F2F4F7] px-4 py-[10px] bg-white gap-4"
+                  style={{ boxShadow: '0px 4px 6px -2px #10182808' }}>
+                  <Paragraph
+                    className="text-[#344054] text-sm leading-[21px] font-bold"
+                    onClick={() => setShowMenu(!showMenu)}>
+                    채용
+                  </Paragraph>
+                  <Paragraph
+                    className="text-[#344054] text-sm leading-[21px] font-medium"
+                    onClick={() => setShowMenu(!showMenu)}>
+                    해외 개발자 원격 채용
+                  </Paragraph>
+                  <Paragraph
+                    className="text-[#344054] text-sm leading-[21px] font-medium"
+                    onClick={() => setShowMenu(!showMenu)}>
+                    외국인 원격 채용 (비개발 직군)
+                  </Paragraph>
+                  <Paragraph
+                    className="text-[#344054] text-sm leading-[21px] font-medium"
+                    onClick={() => setShowMenu(!showMenu)}>
+                    한국어 가능 외국인 채용
+                  </Paragraph>
+                </motion.div>
+              )}
+            </AnimatePresence>
           </Box>
           <Paragraph className="text-base leading-6 font-black text-white">해외 개발자 활용 서비스</Paragraph>
         </Box>
